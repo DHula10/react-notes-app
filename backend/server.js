614b6abbfd0ff0eabf02954c0f5d1135fd3bb204
DHula10 <hula.dominik@seznam.cz>
2026-08-28 14:20:28 +0200

    Edit Folder Structure, Create router

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
-const express = require("express");
+import express from ("express")
+import notesRoutes from "./routes/notesRoutes";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-  res.status(200).send("you have 5 notes");
-});
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
   console.log("Server started on Port: 5001");
