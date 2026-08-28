e06c3dc1500933b0c09b123d86170e023ec6ff76
DHula10 <hula.dominik@seznam.cz>
2026-08-28 13:59:01 +0200

    Add Nodemon package

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/api/notes", (req, res) => {
+  res.status(200).send("you have 5 notes");
+});
+
 app.listen(5001, () => {
-  console.log("Server started on Port 5001");
+  console.log("Server started on Port: 5001");
 });
